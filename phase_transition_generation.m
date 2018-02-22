45e65f152ae4443e2901c66b7c22f3e6c13e8809
Praneeth Narayanamurthy <praneeth.kurpad@gmail.com>
2018-02-21 18:54:10 -0600

    updated phase transition

--- phase_transition_generation.m
+++ phase_transition_generation.m
@@ -1,42 +1,25 @@
-%%%Code to generate the comparison of RPCA algorithms for simulated data
+%%%Wrapper to obtain a phase transition matrix to show the "constant"
+%%%fraction of outlier bounds for NORST while showing standard RPCA
+%%%techniues can only tolerate 1/r.
 
-
-clear;
-clc;
+%clear;
+%clc;
 close all
 
 addpath('YALL1_v1.4')
 
-% pctrunonall warning_off_etc
-
-
-%% Data Generationpctrunonall warning_off_etc
-
+%% Parameter Initialization
 n = 1000;
-t_max = 12000;
+t_max = 11000;
 t_train = 100;
-miss_s = 0;
-miss_s_pca = 0;
 alpha = 300;
 f = 100;
 MC = 1;
 
 t_calc_pca = [alpha - 1 : alpha : t_max - t_train];
 
-sigma = 1e-5;
-theta_degree = 15;
-
-t_reprocs_off = 0;
-%t_ncrpca = 0;
-
-%outfracrowrange = [0.01, 0.3, 0.6];
-outfracrowrange = 0.01;
-rrange = [3, 5];
-%rrange = [3, 10, 30];
-%rrange = ceil(linspace(3, 30, 5));
-
-% PhaseTransNORST = zeros(length(outfracrowrange), length(rrange));
-% PhaseTransAltProj = zeros(length(outfracrowrange), length(rrange));
+outfracrowrange = linspace(0.01, 0.7, 3);
+rrange = ceil(linspace(3, 30, 3));
 
 PhaseTransNORST = zeros(length(outfracrowrange), length(rrange), MC);
 PhaseTransAltProj = zeros(length(outfracrowrange), length(rrange), MC);
@@ -45,19 +28,13 @@ ttall = tic;
 
 for bb  = 1 : length(outfracrowrange)
     b0 = outfracrowrange(bb);
-    
     for rr = 1 : length(rrange)
         r_0 = rrange(rr);
-        
-        %         temp_err_L_off = zeros(MC, t_max - t_train);
-        %         temp_err_L_ncrpca = zeros(MC, t_max- t_train);
-        %
-        %         temp_SE_reprocs_off = zeros(MC, ceil((t_max- t_train)/alpha) - 1);
-        %         temp_SE_ncrpca = zeros(MC, ceil((t_max- t_train)/alpha) - 1);
         parfor mc = 1 : MC
             fprintf('Monte-Carlo iteration %d\t b0 %.2f\t r %d', mc, b0, r_0);
             
             %%%Generating support set and sparse vectors
+            %Using bernoulli model since AltProj fails on Moving Object
             T = zeros(n, t_max);
             rho_train = 0.02;
             rho = b0;
@@ -69,21 +46,17 @@ for bb  = 1 : length(outfracrowrange)
             T(:, t_train + 1 : end) = 1 .* (BernMat(:, t_train + 1 : t_max) <= rho);
             S = (x_min + (x_max - x_min) * rand(n, t_max)) .* T;
             
-            
-            
             %%%Generate low-rank matrix
             %r_0 = 30;
             r_1 = 0;
             r_2 = 0;
             r = r_0;
             L = zeros(n, t_max);
-            %     diag_entries = [linspace(sqrt(f), sqrt(f)/2, r_0 - r_1), ...
-            %         ones(1 , r_1)];
             
             diag_entries = [linspace(sqrt(f), sqrt(f)/2, r_0)];
             t_1 = 3000;
             t_2 = 8000;
-            P = orth(randn(n, r_0));
+            
             coeff_train = zeros(r_0, t_max);
             
             for cc = 1 : r_0
@@ -91,14 +64,15 @@ for bb  = 1 : length(outfracrowrange)
                     2 * diag_entries(cc) * rand(1, t_max);
             end
             
+            %%Generate Subspaces
             Btemp1 = randn(n);
             B1 = (Btemp1 - Btemp1')/2;
             Btemp2 = randn(n);
             B2 = (Btemp2 - Btemp2')/2;
             
-            delta1 = .5e-3;
+            delta1 = .5e-2;
             delta2 = 0.8 * delta1;
-            
+            P = orth(randn(n, r_0));
             PP1 = expm(delta1 * B1)  * P;
             PP2 = expm(delta2 * B2) * PP1;
             
@@ -107,117 +81,46 @@ for bb  = 1 : length(outfracrowrange)
             L(:, t_2 + 1 : end) = PP2 * coeff_train(:, t_2+1:end);
             M = L + S;
             
-            %% Main online robust PCA algorithm section
+            %% Calls to RPCA algorithms
             
-            %%%Algorithm parameters
+            %%%Algorithm parameters for NORST
             K = 8;
             omega = x_min / 2;
             ev_thresh = 7.5961e-04;
             
             %%%Call to offline NORST
             fprintf('\tOffline NORST\t');
-            %             tt7 = tic;
             P_init = orth(ncrpca(M(:, 1 : t_train), r_0, 1e-2, 15));
             [L_hat_off, P_hat_off, S_hat_off, T_hat_off, t_hat_off, ...
-                P_track_full_off, P_track_new_off] = ...
-                Offline_NORST(M(:, t_train + 1 :end),...
-                P_init, ev_thresh, alpha, K, omega);
-            %             t_reprocs_off = t_reprocs_off + toc(tt7);
+                P_track_full_off, P_track_new_off] = Offline_NORST(...
+                M(:, t_train + 1 :end), P_init, ev_thresh, alpha, K, omega);
             
             %%Call to AltProj
             fprintf('AltProj\n');
-            [L_hat_ncrpca, S_hat_ncrpca] = ncrpca(M(:, t_train + 1 : end), 3 * r_0, 1e-6, 100);
+            [L_hat_ncrpca, S_hat_ncrpca] = ...
+                ncrpca(M(:, t_train + 1 : end), 3 * r_0, 1e-6, 100);
             
-            %%Compute performance metrics
+            %% Compute performance metrics
+%             temp_err_L_ncrpca = sqrt(mean((L(:, t_train + 1 :end) - ...
+%                 L_hat_ncrpca).^2, 1)) ./ sqrt(mean(L(:, t_train + 1 :end).^2, 1));
+%             temp_err_L_norst = sqrt(mean((L(:, t_train + 1 :end) - ...
+%                 L_hat_off).^2, 1)) ./ sqrt(mean(L(:, t_train + 1 :end).^2, 1));
             
-            temp_SE_reprocs_off = zeros(1, length(t_calc_pca));
-            temp_SE_ncrpca = zeros(1, length(t_calc_pca));
-            for jj = 1 : length(t_calc_pca)
-                P_hat_ncrpca = orth(L_hat_ncrpca(:, 1 : jj));
-                if (t_calc_pca(jj) +t_train < t_1)
-                    temp_SE_reprocs_off(jj) = ...
-                        Calc_SubspaceError(P_track_full_off{1}, ...
-                        P(:, 1:r_0));
-                    temp_SE_ncrpca(jj) = ...
-                        Calc_SubspaceError(P_hat_ncrpca, ...
-                        P(:, 1 :r_0));
-                elseif((t_calc_pca(jj) +t_train >= t_1) && (t_calc_pca(jj) + t_train < t_2))
-                    temp_SE_reprocs_off(jj) = ...
-                        Calc_SubspaceError(P_track_full_off{2}, PP1);
-                    temp_SE_ncrpca(jj) = ...
-                        Calc_SubspaceError(P_hat_ncrpca, PP1);
-                else
-                    temp_SE_reprocs_off(jj) = ...
-                        Calc_SubspaceError(P_track_full_off{3}, PP2);
-                    temp_SE_ncrpca(jj) = ...
-                        Calc_SubspaceError(P_hat_ncrpca, PP2);
-                end
-            end
+            PhaseTransAltProj(rr, bb, mc) = ...
+                norm(L(:, t_train + 1 : end) - L_hat_ncrpca, 'fro') / ...
+                norm(L(:, t_train + 1 : end), 'fro');
+            PhaseTransNORST(rr, bb, mc) = ...
+                norm(L(:, t_train + 1 : end) - L_hat_off, 'fro') / ...
+                norm(L(:, t_train + 1 : end), 'fro');
             
-%             temp_SE_NORST(mc) = mean(temp_SE_reprocs_off);
-%             temp1_SE_ncrpca(mc) = mean(temp_SE_ncrpca);
-            
-            PhaseTransNORST(rr, bb, mc) = mean(temp_SE_reprocs_off);
-            PhaseTransAltProj(rr, bb, mc) = mean(temp_SE_ncrpca);
-            
-            
+%             PhaseTransNORST(rr, bb, mc) = mean(temp_err_L_norst);
+%             PhaseTransAltProj(rr, bb, mc) = mean(temp_err_L_ncrpca);
         end
-        
-        %         err_L_off = mean(temp_err_L_off, 1);
-        %err_L_ncrpca = mean(temp_err_L_ncrpca, 1);
-        
-        %         err_SE_reprocs_off = mean(temp_SE_reprocs_off, 1);
-        %err_SE_ncrpca = mean(temp_SE_ncrpca, 1);
-        
-%         PhaseTransNORST(bb, rr) = mean(temp_SE_NORST);
-%         PhaseTransAltProj(bb, rr) = mean(temp1_SE_ncrpca);
     end
 end
 fprintf('\n')
 toc(ttall)
 
+%save('phase_trans_b0_r_LF.mat');
 
-
-%t_reprocs_off = t_reprocs_off / (MC * t_max)
-% t_ncrpca = t_ncrpca / (MC * t_max)
-
-
-% figure;
-% subplot(311);
-% imagesc(S);
-% subplot(312);
-% imagesc(S_hat_pca);
-% subplot(313);
-% imagesc(S_hat_off);
-
-% stry ='$$SE(\hat{P}, P)$$';
-% strx = '$$t$$';
-% figure
-% plot(t_calc_pca, log10(err_SE_reprocs_off), 'gs--', 'LineWidth', 2)
-% % plot(t_calc_pca, log10(temp_SE_reprocs_pca(3, :)), 'ks--', 'LineWidth', 2)
-% % plot(t_calc_pca, log10(temp_SE_reprocs_off(1, :)), 'b^-', 'LineWidth', 2)
-% % plot(t_calc_pca, log10(temp_SE_reprocs_off(2, :)), 'g^-', 'LineWidth', 2)
-% % plot(t_calc_pca, log10(temp_SE_reprocs_off(3, :)), 'k^-', 'LineWidth', 2)
-%
-% % plot(t_calc, log10(err_SE_ncrpca), 'g', 'LineWidth', 2)
-% xlabel(strx, 'Interpreter', 'LaTeX', 'FontSize', 14);
-% ylabel(stry, 'Interpreter', 'LaTeX', 'FontSize', 14);
-% axis tight
-%legend('NORST x_{min} = 0.5', 'NORST x_{min} = 5', 'NORST x_{min} = 10', ...
-%'Offline x_{min} = 0.5', 'Offline x_{min} = 5', 'Offline x_{min} = 10');
-%title('NORST')
-
-
-% stry ='$$SE(\hat{P}, P)$$';
-% strx = '$$t$$';
-% figure
-% plot(t_calc_pca, log10(temp_SE_reprocs_off(1, :)), 'bs--', 'LineWidth', 2)
-% hold
-% plot(t_calc_pca, log10(temp_SE_reprocs_off(2, :)), 'gs--', 'LineWidth', 2)
-% plot(t_calc_pca, log10(temp_SE_reprocs_off(3, :)), 'ks--', 'LineWidth', 2)
-% % plot(t_calc, log10(err_SE_ncrpca), 'g', 'LineWidth', 2)
-% xlabel(strx, 'Interpreter', 'LaTeX', 'FontSize', 14);
-% ylabel(stry, 'Interpreter', 'LaTeX', 'FontSize', 14);
-% axis tight
-% legend('x_min = 1', 'x_min = 5', 'x_min = 10');
-% title('Offline NORST')
+% save('data_TIT/phase_trans_b0_r.mat');
--- xmin_variation.m
+++ xmin_variation.m
@@ -1,5 +1,5 @@
-%%%Code to generate the comparison of RPCA algorithms for simulated data
-
+%%%Wrapper to verify the variation of NORST and ALtProj with x_min, the
+%%%smallest outlier-magnitude.
 
 clear;
 clc;
@@ -7,7 +7,7 @@ close all
 
 addpath('YALL1_v1.4')
 
-%% Data Generation
+%% Parameter Initialization
 n = 1000;
 t_max = 12000;
 t_train = 100;
@@ -17,22 +17,6 @@ alpha = 300;
 f = 100;
 MC = 100;
 
-sigma = 1e-5;
-theta_degree = 15;
-
-t_reprocs_pca = 0;
-t_reprocs_off = 0;
-%t_ncrpca = 0;
-
-
-% temp_err_L_pca = zeros(MC, t_max - t_train);
-% temp_err_L_off = zeros(MC, t_max - t_train);
-% temp_err_L_ncrpca = zeros(MC, t_max- t_train);
-%
-% temp_SE_reprocs_pca = zeros(MC, ceil((t_max- t_train)/alpha) - 1);
-% temp_SE_reprocs_off = zeros(MC, ceil((t_max- t_train)/alpha) - 1);
-% temp_SE_ncrpca = zeros(MC, ceil((t_max- t_train)/alpha) - 1);
-
 xminBnd = [.5, 5, 10];
 temp_SE_reprocs_pca = zeros(length(xminBnd), ceil((t_max- t_train)/alpha) - 1, MC);
 temp_SE_reprocs_off = zeros(length(xminBnd), ceil((t_max- t_train)/alpha) - 1, MC);
@@ -40,25 +24,10 @@ temp_SE_ncrpca = zeros(length(xminBnd), ceil((t_max- t_train)/alpha) - 1, MC);
 
 
 ttall = tic;
-
-
 for mc = 1 : MC
-    
-    %     fprintf('Monte-Carlo iteration %d in progress\n', mc);
-    
-    
-    
     temp_err_L_pca = zeros(length(xminBnd), t_max - t_train);
     temp_err_L_off = zeros(length(xminBnd), t_max - t_train);
     temp_err_L_ncrpca = zeros(length(xminBnd), t_max- t_train);
-    
-%     temp_SE_reprocs_pca = zeros(length(xminBnd), ceil((t_max- t_train)/alpha) - 1);
-%     temp_SE_reprocs_off = zeros(length(xminBnd), ceil((t_max- t_train)/alpha) - 1);
-%     temp_SE_ncrpca = zeros(length(xminBnd), ceil((t_max- t_train)/alpha) - 1);
-    
-    
-    
-    
     for xx = 1 : length(xminBnd)
         x_min = xminBnd(xx);
         
@@ -149,23 +118,10 @@ for mc = 1 : MC
             end
             idx = bind : sind;
             jdx = t_train + (ii-1) * beta + 1 : t_train + ii * beta;
-%             S(idx, jdx) = x_min + ...
-%                 (x_max - x_min) * rand(length(idx), beta);
-            S(idx, jdx) = x_min * ones(length(idx), beta); 
+            %setting all entries to x_min to make things harder!
+            S(idx, jdx) = x_min * ones(length(idx), beta);
             T(idx, jdx) = 1;
         end
-        
-        %     rho_train = 0.02;
-        %     rho = 0.3;
-        %     x_min = 10;
-        %     x_max = 20;
-        %
-        %     BernMat = rand(n, t_max);
-        %     T(:, 1 : t_train) = 1.* (BernMat(:, 1 : t_train) <= rho_train);
-        %     T(:, t_train + 1 : end) = 1 .* (BernMat(:, t_train + 1 : t_max) <= rho);
-        %     S = (x_min + (x_max - x_min) * rand(n, t_max)) .* T;
-        
-        
         
         %%%Generate low-rank matrix
         r_0 = 30;
@@ -173,13 +129,10 @@ for mc = 1 : MC
         r_2 = 0;
         r = r_0;
         L = zeros(n, t_max);
-        %     diag_entries = [linspace(sqrt(f), sqrt(f)/2, r_0 - r_1), ...
-        %         ones(1 , r_1)];
         
         diag_entries = [linspace(sqrt(f), sqrt(f)/2, r_0)];
         t_1 = 3000;
         t_2 = 8000;
-        P = orth(randn(n, r_0));
         coeff_train = zeros(r_0, t_max);
         
         for cc = 1 : r_0
@@ -195,6 +148,7 @@ for mc = 1 : MC
         delta1 = .5e-3;
         delta2 = 0.8 * delta1;
         
+        P = orth(randn(n, r_0));
         PP1 = expm(delta1 * B1)  * P;
         PP2 = expm(delta2 * B2) * PP1;
         
@@ -203,7 +157,7 @@ for mc = 1 : MC
         L(:, t_2 + 1 : end) = PP2 * coeff_train(:, t_2+1:end);
         M = L + S;
         
-        %% Main online robust PCA algorithm section
+        %% Calls to NORST, Offline NORST and AltProj
         
         %%%Algorithm parameters
         K = 8;
@@ -211,33 +165,27 @@ for mc = 1 : MC
         %     gamma = sqrt(4 * log(n)/n);
         %     s = ceil((gamma + rho) * n);
         
-        %%%Call to ReProCS-PCA
+        %%%Call to NORST
         fprintf('NORST\t');
         ev_thresh = 7.5961e-04;
-        tt9 = tic;
         P_init = orth(ncrpca(M(:, 1 : t_train), r_0, 1e-2, 15));
         [L_hat_pca, P_hat_pca, S_hat_pca, T_hat_pca, t_hat_pca, ...
             P_track_full_pca, t_calc_pca] = ...
             NORST(M(:, t_train + 1 :end),...
             P_init, ev_thresh, alpha, K, omega);
-        t_reprocs_pca = t_reprocs_pca + toc(tt9);
         
-        %%%Call to offline ReProCS
-        %     ev_thresh = 1e-3;
-        %gamma = sqrt(5 * log(n) / n);
+        %%%Call to offline NORST
         fprintf('Offline NORST\n');
-        tt7 = tic;
         P_init = orth(ncrpca(M(:, 1 : t_train), r_0, 1e-2, 15));
         [L_hat_off, P_hat_off, S_hat_off, T_hat_off, t_hat_off, ...
             P_track_full_off, P_track_new_off] = ...
             Offline_NORST(M(:, t_train + 1 :end),...
             P_init, ev_thresh, alpha, K, omega);
-        t_reprocs_off = t_reprocs_off + toc(tt7);
         
         %%Call to AltProj
-            fprintf('AltProj\t');
-            [L_hat_ncrpca, S_hat_ncrpca] = ...
-                ncrpca(M(:, t_train + 1 : end), r_0 + r_1 + r_2, 1e-6, 100);
+        fprintf('AltProj\t');
+        [L_hat_ncrpca, S_hat_ncrpca] = ...
+            ncrpca(M(:, t_train + 1 : end), r_0 + r_1 + r_2, 1e-6, 100);
         
         %%Compute performance metrics
         temp_err_L_pca(xx, :) = ...
@@ -246,8 +194,8 @@ for mc = 1 : MC
         temp_err_L_off(xx, :) = ...
             sqrt(mean((L(:, t_train+1:end) - L_hat_off).^2, 1)) ...
             ./ sqrt(mean(L(:, t_train+1:end).^2, 1));
-            temp_err_L_ncrpca(xx, :) = sqrt(mean((L(:, t_train + 1 :end) - ...
-                L_hat_ncrpca).^2, 1)) ./ sqrt(mean(L(:, t_train + 1 :end).^2, 1));
+        temp_err_L_ncrpca(xx, :) = sqrt(mean((L(:, t_train + 1 :end) - ...
+            L_hat_ncrpca).^2, 1)) ./ sqrt(mean(L(:, t_train + 1 :end).^2, 1));
         
         %     miss_s = ...
         %         miss_s + (length(find(S_hat_off))- length(find(S)))/numel(S);
@@ -265,23 +213,23 @@ for mc = 1 : MC
                 temp_SE_reprocs_off(xx, jj, mc) = ...
                     Calc_SubspaceError(P_track_full_off{1}, ...
                     P(:, 1:r_0));
-                            temp_SE_ncrpca(xx, jj, mc) = ...
-                                Calc_SubspaceError(P_hat_ncrpca, ...
-                                P(:, 1 :r_0));
+                temp_SE_ncrpca(xx, jj, mc) = ...
+                    Calc_SubspaceError(P_hat_ncrpca, ...
+                    P(:, 1 :r_0));
             elseif((t_calc_pca(jj) +t_train >= t_1) && (t_calc_pca(jj) + t_train < t_2))
                 temp_SE_reprocs_pca(xx, jj, mc) = ...
                     Calc_SubspaceError(P_track_full_pca{jj}, PP1);
                 temp_SE_reprocs_off(xx, jj, mc) = ...
                     Calc_SubspaceError(P_track_full_off{2}, PP1);
-                            temp_SE_ncrpca(xx, jj, mc) = ...
-                                Calc_SubspaceError(P_hat_ncrpca, PP1);
+                temp_SE_ncrpca(xx, jj, mc) = ...
+                    Calc_SubspaceError(P_hat_ncrpca, PP1);
             else
                 temp_SE_reprocs_pca(xx, jj, mc) = ...
                     Calc_SubspaceError(P_track_full_pca{jj}, PP2);
                 temp_SE_reprocs_off(xx, jj, mc) = ...
                     Calc_SubspaceError(P_track_full_off{3}, PP2);
-                            temp_SE_ncrpca(xx, jj, mc) = ...
-                                Calc_SubspaceError(P_hat_ncrpca, PP2);
+                temp_SE_ncrpca(xx, jj, mc) = ...
+                    Calc_SubspaceError(P_hat_ncrpca, PP2);
             end
         end
         
@@ -290,66 +238,4 @@ end
 fprintf('\n')
 toc(ttall)
 
-%err_L_pca = mean(temp_err_L_pca, 1);
-%err_L_off = mean(temp_err_L_off, 1);
-% err_L_ncrpca = mean(temp_err_L_ncrpca, 1);
-
-%err_SE_reprocs_pca = mean(temp_SE_reprocs_pca, 1);
-%err_SE_reprocs_off = mean(temp_SE_reprocs_off, 1);
-% err_SE_ncrpca = mean(temp_SE_ncrpca, 1);
-
-t_reprocs_pca = t_reprocs_pca / (MC * t_max)
-t_reprocs_off = t_reprocs_off / (MC * t_max)
-% t_ncrpca = t_ncrpca / (MC * t_max)
-
-save('xmin_variation_mc100.mat')
-
-
-% figure;
-% subplot(311);
-% imagesc(S);
-% subplot(312);
-% imagesc(S_hat_pca);
-% subplot(313);
-% imagesc(S_hat_off);
-% 
-% %t_calc_pca = [ alpha - 1 : alpha : t_max - t_train];
-% 
-% stry ='$$SE(\hat{P}, P)$$';
-% strx = '$$t$$';
-% figure
-% plot(t_calc_pca, log10(temp_SE_reprocs_pca(1, :)), 'bs--', 'LineWidth', 2)
-% hold
-% plot(t_calc_pca, log10(temp_SE_reprocs_pca(2, :)), 'gs--', 'LineWidth', 2)
-% plot(t_calc_pca, log10(temp_SE_reprocs_pca(3, :)), 'ks--', 'LineWidth', 2)
-% plot(t_calc_pca, log10(temp_SE_reprocs_off(1, :)), 'b^-', 'LineWidth', 2)
-% plot(t_calc_pca, log10(temp_SE_reprocs_off(2, :)), 'g^-', 'LineWidth', 2)
-% plot(t_calc_pca, log10(temp_SE_reprocs_off(3, :)), 'k^-', 'LineWidth', 2)
-% plot(t_calc_pca, log10(temp_SE_ncrpca(1, :)), 'bd-.', 'LineWidth', 2)
-% plot(t_calc_pca, log10(temp_SE_ncrpca(2, :)), 'gd-.', 'LineWidth', 2)
-% plot(t_calc_pca, log10(temp_SE_ncrpca(3, :)), 'kd-.', 'LineWidth', 2)
-% 
-% 
-% % plot(t_calc, log10(err_SE_ncrpca), 'g', 'LineWidth', 2)
-% xlabel(strx, 'Interpreter', 'LaTeX', 'FontSize', 14);
-% ylabel(stry, 'Interpreter', 'LaTeX', 'FontSize', 14);
-% axis tight
-% legend('NORST x_{min} = 0.5', 'NORST x_{min} = 5', 'NORST x_{min} = 10', ...
-%     'Offline x_{min} = 0.5', 'Offline x_{min} = 5', 'Offline x_{min} = 10', ...
-%     'AltProj x_{min} = 0.5', 'AltProj x_{min} = 5', 'AltProj x_{min} = 10');
-%title('NORST')
-
-
-% stry ='$$SE(\hat{P}, P)$$';
-% strx = '$$t$$';
-% figure
-% plot(t_calc_pca, log10(temp_SE_reprocs_off(1, :)), 'bs--', 'LineWidth', 2)
-% hold
-% plot(t_calc_pca, log10(temp_SE_reprocs_off(2, :)), 'gs--', 'LineWidth', 2)
-% plot(t_calc_pca, log10(temp_SE_reprocs_off(3, :)), 'ks--', 'LineWidth', 2)
-% % plot(t_calc, log10(err_SE_ncrpca), 'g', 'LineWidth', 2)
-% xlabel(strx, 'Interpreter', 'LaTeX', 'FontSize', 14);
-% ylabel(stry, 'Interpreter', 'LaTeX', 'FontSize', 14);
-% axis tight
-% legend('x_min = 1', 'x_min = 5', 'x_min = 10');
-% title('Offline NORST')
+save('data_TIT/xmin_variation_mc100.mat')
